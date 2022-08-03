e6971dd7c364806063e8ea309a026bd4c1bd3b85
aquiro02 <108485454+aquiro02@users.noreply.github.com>
2022-08-03 15:25:56 -0400

    Update Note_Sequencer.py

--- Musical Instrument Code/Hub Sounds/Note_Sequencer.py
+++ Musical Instrument Code/Hub Sounds/Note_Sequencer.py
@@ -1,5 +1,9 @@
+# By Samson Bienstock
+# FET Lab Summer 2022
+# Written for Spike 2 in the Spike Web App
+
 #Play function
-#SAMSON BIENSTOCK inspired by CHRIS ROGERS
+
 def NoteSequencer() :
     #start program 
     print("starting in 3....2.....1.....")
